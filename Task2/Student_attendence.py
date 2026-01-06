9ab2c306dac050945e2312b2ba5f229f33a49043
Pranjal <pranjal10092005@gmail.com>
2026-01-06 22:08:22 +0530

    day 2 task no 9

---
+++ Task2/Student_attendence.py
@@ -0,0 +1,233 @@
+Data = attendance = [
+    ("Ajay",   "2026-01-01", "Present"),
+    ("Ajay",   "2026-01-02", "Present"),
+    ("Ajay",   "2026-01-03", "Absent"),
+    ("Ajay",   "2026-01-04", "Present"),
+    ("Ajay",   "2026-01-05", "Present"),
+    ("Ajay",   "2026-01-06", "Absent"),
+    ("Ajay",   "2026-01-07", "Present"),
+    ("Ajay",   "2026-01-08", "Present"),
+    ("Ajay",   "2026-01-09", "Present"),
+    ("Ajay",   "2026-01-10", "Absent"),
+    ("Ajay",   "2026-01-11", "Present"),
+    ("Ajay",   "2026-01-12", "Present"),
+    ("Ajay",   "2026-01-13", "Present"),
+    ("Ajay",   "2026-01-14", "Absent"),
+    ("Ajay",   "2026-01-15", "Present"),
+
+    ("Rahul",  "2026-01-01", "Present"),
+    ("Rahul",  "2026-01-02", "Present"),
+    ("Rahul",  "2026-01-03", "Present"),
+    ("Rahul",  "2026-01-04", "Present"),
+    ("Rahul",  "2026-01-05", "Present"),
+    ("Rahul",  "2026-01-06", "Present"),
+    ("Rahul",  "2026-01-07", "Absent"),
+    ("Rahul",  "2026-01-08", "Present"),
+    ("Rahul",  "2026-01-09", "Present"),
+    ("Rahul",  "2026-01-10", "Present"),
+    ("Rahul",  "2026-01-11", "Present"),
+    ("Rahul",  "2026-01-12", "Absent"),
+    ("Rahul",  "2026-01-13", "Present"),
+    ("Rahul",  "2026-01-14", "Present"),
+    ("Rahul",  "2026-01-15", "Present"),
+
+    ("Neha",   "2026-01-01", "Absent"),
+    ("Neha",   "2026-01-02", "Present"),
+    ("Neha",   "2026-01-03", "Present"),
+    ("Neha",   "2026-01-04", "Absent"),
+    ("Neha",   "2026-01-05", "Present"),
+    ("Neha",   "2026-01-06", "Present"),
+    ("Neha",   "2026-01-07", "Present"),
+    ("Neha",   "2026-01-08", "Absent"),
+    ("Neha",   "2026-01-09", "Present"),
+    ("Neha",   "2026-01-10", "Present"),
+    ("Neha",   "2026-01-11", "Absent"),
+    ("Neha",   "2026-01-12", "Present"),
+    ("Neha",   "2026-01-13", "Present"),
+    ("Neha",   "2026-01-14", "Present"),
+    ("Neha",   "2026-01-15", "Absent"),
+
+    ("Amit",   "2026-01-01", "Present"),
+    ("Amit",   "2026-01-02", "Absent"),
+    ("Amit",   "2026-01-03", "Absent"),
+    ("Amit",   "2026-01-04", "Present"),
+    ("Amit",   "2026-01-05", "Present"),
+    ("Amit",   "2026-01-06", "Present"),
+    ("Amit",   "2026-01-07", "Absent"),
+    ("Amit",   "2026-01-08", "Present"),
+    ("Amit",   "2026-01-09", "Present"),
+    ("Amit",   "2026-01-10", "Absent"),
+    ("Amit",   "2026-01-11", "Present"),
+    ("Amit",   "2026-01-12", "Absent"),
+    ("Amit",   "2026-01-13", "Present"),
+    ("Amit",   "2026-01-14", "Present"),
+    ("Amit",   "2026-01-15", "Present"),
+
+    ("Sneha",  "2026-01-01", "Present"),
+    ("Sneha",  "2026-01-02", "Present"),
+    ("Sneha",  "2026-01-03", "Present"),
+    ("Sneha",  "2026-01-04", "Absent"),
+    ("Sneha",  "2026-01-05", "Absent"),
+    ("Sneha",  "2026-01-06", "Present"),
+    ("Sneha",  "2026-01-07", "Present"),
+    ("Sneha",  "2026-01-08", "Present"),
+    ("Sneha",  "2026-01-09", "Absent"),
+    ("Sneha",  "2026-01-10", "Present"),
+    ("Sneha",  "2026-01-11", "Present"),
+    ("Sneha",  "2026-01-12", "Present"),
+    ("Sneha",  "2026-01-13", "Absent"),
+    ("Sneha",  "2026-01-14", "Present"),
+    ("Sneha",  "2026-01-15", "Present"),
+
+    ("Rohit",  "2026-01-01", "Absent"),
+    ("Rohit",  "2026-01-02", "Absent"),
+    ("Rohit",  "2026-01-03", "Present"),
+    ("Rohit",  "2026-01-04", "Present"),
+    ("Rohit",  "2026-01-05", "Present"),
+    ("Rohit",  "2026-01-06", "Absent"),
+    ("Rohit",  "2026-01-07", "Absent"),
+    ("Rohit",  "2026-01-08", "Present"),
+    ("Rohit",  "2026-01-09", "Present"),
+    ("Rohit",  "2026-01-10", "Present"),
+    ("Rohit",  "2026-01-11", "Absent"),
+    ("Rohit",  "2026-01-12", "Present"),
+    ("Rohit",  "2026-01-13", "Present"),
+    ("Rohit",  "2026-01-14", "Absent"),
+    ("Rohit",  "2026-01-15", "Present"),
+
+    ("Pooja",  "2026-01-01", "Present"),
+    ("Pooja",  "2026-01-02", "Present"),
+    ("Pooja",  "2026-01-03", "Absent"),
+    ("Pooja",  "2026-01-04", "Present"),
+    ("Pooja",  "2026-01-05", "Absent"),
+    ("Pooja",  "2026-01-06", "Present"),
+    ("Pooja",  "2026-01-07", "Present"),
+    ("Pooja",  "2026-01-08", "Absent"),
+    ("Pooja",  "2026-01-09", "Present"),
+    ("Pooja",  "2026-01-10", "Present"),
+    ("Pooja",  "2026-01-11", "Present"),
+    ("Pooja",  "2026-01-12", "Absent"),
+    ("Pooja",  "2026-01-13", "Present"),
+    ("Pooja",  "2026-01-14", "Present"),
+    ("Pooja",  "2026-01-15", "Present"),
+
+    ("Kunal",  "2026-01-01", "Present"),
+    ("Kunal",  "2026-01-02", "Absent"),
+    ("Kunal",  "2026-01-03", "Present"),
+    ("Kunal",  "2026-01-04", "Present"),
+    ("Kunal",  "2026-01-05", "Present"),
+    ("Kunal",  "2026-01-06", "Absent"),
+    ("Kunal",  "2026-01-07", "Present"),
+    ("Kunal",  "2026-01-08", "Present"),
+    ("Kunal",  "2026-01-09", "Absent"),
+    ("Kunal",  "2026-01-10", "Present"),
+    ("Kunal",  "2026-01-11", "Present"),
+    ("Kunal",  "2026-01-12", "Present"),
+    ("Kunal",  "2026-01-13", "Absent"),
+    ("Kunal",  "2026-01-14", "Present"),
+    ("Kunal",  "2026-01-15", "Present"),
+
+    ("Anita",  "2026-01-01", "Present"),
+    ("Anita",  "2026-01-02", "Present"),
+    ("Anita",  "2026-01-03", "Present"),
+    ("Anita",  "2026-01-04", "Absent"),
+    ("Anita",  "2026-01-05", "Present"),
+    ("Anita",  "2026-01-06", "Present"),
+    ("Anita",  "2026-01-07", "Absent"),
+    ("Anita",  "2026-01-08", "Present"),
+    ("Anita",  "2026-01-09", "Present"),
+    ("Anita",  "2026-01-10", "Present"),
+    ("Anita",  "2026-01-11", "Present"),
+    ("Anita",  "2026-01-12", "Absent"),
+    ("Anita",  "2026-01-13", "Present"),
+    ("Anita",  "2026-01-14", "Present"),
+    ("Anita",  "2026-01-15", "Present"),
+
+    ("Vikas",  "2026-01-01", "Absent"),
+    ("Vikas",  "2026-01-02", "Present"),
+    ("Vikas",  "2026-01-03", "Absent"),
+    ("Vikas",  "2026-01-04", "Present"),
+    ("Vikas",  "2026-01-05", "Present"),
+    ("Vikas",  "2026-01-06", "Absent"),
+    ("Vikas",  "2026-01-07", "Present"),
+    ("Vikas",  "2026-01-08", "Absent"),
+    ("Vikas",  "2026-01-09", "Present"),
+    ("Vikas",  "2026-01-10", "Present"),
+    ("Vikas",  "2026-01-11", "Absent"),
+    ("Vikas",  "2026-01-12", "Present"),
+    ("Vikas",  "2026-01-13", "Present"),
+    ("Vikas",  "2026-01-14", "Absent"),
+    ("Vikas",  "2026-01-15", "Present"),
+]
+
+def make_table(Data):
+    names = sorted({name for name, _, _ in Data})
+    dates = sorted({date for _, date, _ in Data})
+    
+    rows = len(names)
+    cols = len(dates)
+    
+    table = [["NA"] * cols for _ in range(rows)]
+    
+    for i in range(rows):
+        for j in range(cols):
+            for name, date, status in Data:
+                if name == names[i] and date == dates[j]:
+                    table[i][j] = "P" if status == "Present" else "A"
+                    
+    return names, dates, table
+
+def get_percentage(table, dates, names):
+    attendance_percentage = {}
+
+    total_days = len(dates)
+
+    for i in range(len(names)):
+        present_count = 0
+
+        for j in range(len(dates)):
+            if table[i][j] == "P":
+                present_count += 1
+
+        percentage = (present_count / total_days) * 100
+        attendance_percentage[names[i]] = percentage
+
+    return attendance_percentage
+
+def get_max_streak(table, dates, names):
+    
+    max_streak = {}
+    
+    for i in range(len(names)):
+        
+        streak = 0
+        cur_streak = 0
+        for j in range(len(dates)):
+            if table[i][j] == "P":
+                cur_streak = cur_streak + 1
+            else:
+                cur_streak = 0
+            
+            streak = max(streak, cur_streak)
+        max_streak[names[i]] = streak
+    
+    return max_streak
+
+def analyse(Data):
+    names, dates, table = make_table(Data)
+    attendence_percentage = get_percentage(table, dates, names)
+    max_streak = get_max_streak(table, dates, names)
+    
+    report = {}
+
+    for name in names:
+        report[name] = {
+            "attendance_percentage": round(attendence_percentage[name], 2),
+            "max_present_streak": max_streak[name]
+        }
+    
+    return report
+
+result = analyse(Data)
+
+print(result)
